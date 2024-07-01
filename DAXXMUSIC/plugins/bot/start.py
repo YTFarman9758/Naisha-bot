7360682fbc1e52d91e1162a5fdfb862104d7e6b5
YTFarman9758 <162802755+YTFarman9758@users.noreply.github.com>
2024-07-01 13:31:15 +0530

    Update start.py

--- DAXXMUSIC/plugins/bot/start.py
+++ DAXXMUSIC/plugins/bot/start.py
@@ -49,7 +49,7 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            return await message.reply_video(
+            return await message.reply_photo(
                 random.choice(NEXI_VID),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
