7b85d6c728f48da0882da67a90e3103bd7dbefea
YTFarman9758 <162802755+YTFarman9758@users.noreply.github.com>
2026-07-06 09:29:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ ASSUSERNAME = getenv("ASSUSERNAME" , "MREVO")
 # ---------- SHRUTI YOUTUBE API (Naisha Bot Integration) ----------
 # Vars For API End Pont.
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
-YT_API_KEY = getenv("YT_API_KEY" , 'xbit_GjLUhA7Xsu_5Dr_xBdFZLr8LzorcKIkK') ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
+YT_API_KEY = getenv("YT_API_KEY" , 'ShrutiBotsCMJel4aMwKOc8ZHhkWHr') ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
